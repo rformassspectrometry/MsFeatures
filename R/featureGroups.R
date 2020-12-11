765d11d016d9b8ce9dedd15bd366f99b807c3350
jorainer <johannes.rainer@gmail.com>
2020-12-11 12:06:29 +0100

    feat: add an example SummarizedExperiment and add featureGroups

---
+++ R/featureGroups.R
@@ -0,0 +1,22 @@
+#' @rdname featureGroups
+#'
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#'
+#' @importMethodsFrom SummarizedExperiment rowData
+setMethod("featureGroups", "SummarizedExperiment", function(object) {
+    if (any(colnames(rowData(object)) == "feature_group"))
+        as.character(rowData(object)$feature_group)
+    else rep(NA_character_, nrow(rowData(object)))
+})
+
+#' @rdname featureGroups
+#'
+#' @importMethodsFrom SummarizedExperiment rowData<-
+setReplaceMethod(
+    "featureGroups", "SummarizedExperiment", function(object, value) {
+        if (!(length(value) == 1 | length(value) == nrow(rowData(object))))
+            stop("'value' has to be either of length 1 or equal to the number ",
+                 "of rows in object")
+        rowData(object)$feature_group <- as.character(value)
+        object
+    })
---
+++ R/se.R
@@ -0,0 +1,13 @@
+#' @title Quantified LC-MS preprocessing result test data
+#'
+#' @description
+#'
+#' The `se` variable is a [SummarizedExperiment()] object representing the
+#' results from a `xcms`-based pre-processing of an LC-MS untargeted
+#' metabolomics data set. The raw data files are provided in the `faahKO`
+#' package. The pre-processing of this data set is described in detail in
+#' the *xcms* vignette of the `xcms` package. This object was created from the
+#' `XCMSnExp` result object with the `quantify` method.
+#'
+#' @name se
+NULL
